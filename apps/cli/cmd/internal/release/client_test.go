7875547a37dd33f164f161a666e2a7cb99f8cb4a
shiyudesu <ming051017@outlook.com>
2026-08-08 11:33:36 +0800

    Revert "test(cli): demonstrate CI failure detection"
    This reverts commit daceb6c7f0390d7404b04e4af1a738b6e23306fe.

--- apps/cli/cmd/internal/release/client_test.go
+++ apps/cli/cmd/internal/release/client_test.go
@@ -72,7 +72,7 @@ func TestByTag(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if requested != "/deliberately-broken-ci-check" {
+	if requested != "/repos/Open-Source-Kigali/docksight/releases/tags/v0.0.3" {
 		t.Fatalf("requested %q", requested)
 	}
 }
